7eb0a0ad62f0a4214b69e0b523b5a1ef8d29c9ea
Nikita Denisov <nadenisov@gmail.com>
2025-11-20 15:25:27 +0300

    refactor - next interation

--- entrypoint/start-squid.sh
+++ entrypoint/start-squid.sh
@@ -4,12 +4,20 @@ set -eu
 . /entrypoint/functions.sh
 . /entrypoint/env.sh
 
+# Default log file for Squid
+: "${SQUID_LOGFILE:=/var/log/amneziawg/squid.log}"
+
 start_squid() {
-    if [ "${SQUID_ENABLED:-false}" = "true" ]; then
-        info "🦑 Starting Squid proxy on port ${SQUID_PORT}..."
-        # Start Squid service in background (Alpine: service not used, just run squid)
-        squid -N -d 1 -f /etc/squid/squid.conf &
-        success "Squid proxy started"
+    if [ "${SQUID_ENABLE:-false}" = "true" ]; then
+        info "🦑 Starting Squid proxy on port ${SQUID_PORT} (logs: $SQUID_LOGFILE)..."
+
+        # Ensure log directory exists
+        mkdir -p "$(dirname "$SQUID_LOGFILE")"
+
+        # Start Squid in background, redirecting stdout/stderr to log file
+        squid -N -d 1 -f /etc/squid/squid.conf >> "$SQUID_LOGFILE" 2>&1 &
+
+        success "Squid proxy started (check $SQUID_LOGFILE for details)"
     else
         info "Squid proxy disabled"
     fi
