d7268996d6dd29d387c4e4e7994536cd35d434ab
Nikita Denisov <nadenisov@gmail.com>
2025-11-24 15:32:04 +0300

    changed missing database verbosity

--- entrypoint/config-db.sh
+++ entrypoint/config-db.sh
@@ -54,7 +54,7 @@ validate_or_repair_db() {
 
     # DB missing
     if [ ! -f "$CONFIG_DB" ]; then
-        error "Configuration database missing: $CONFIG_DB"
+        warning "Configuration database missing: $CONFIG_DB"
         init_config_db
         return
     fi
